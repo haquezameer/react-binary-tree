a8eff35c861b0eb0a4551b80cf334816f8725a34
zamhaq <haque864@gmail.com>
2018-06-14 07:12:05 +0530

    create tree from assignment data & layout vertically

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
-import Tree from 'react-tree-graph';
-import './App.css';
-// import 'react-tree-graph/dist/style.css'
+import React, { Component } from "react";
+import Tree from "react-tree-graph";
+import "./App.css";
+import "react-tree-graph/dist/style.css";
 let data = {
-  name: 'Parent',
-  children: [{
-      name: 'Child One'
-  }, {
-      name: 'Child Two'
-  }]
+  name: "34",
+  children: [
+    {
+      name: "92"
+    },
+    {
+      name: "23",
+      children: [
+        {
+          name: "04",
+          children: [
+            {
+              name: "09"
+            },
+            { name: "16" }
+          ]
+        },
+        {
+          name: "12"
+        }
+      ]
+    }
+  ]
 };
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Tree data={data} height={400}
-    width={400} circleProps={{fill: 'red'}}/>
-        <div>
-          some numbers
-          </div>
+        <Tree
+          data={data}
+          height={400}
+          width={400}
+          circleProps={{ fill: "red" }}
+          svgProps={{
+            transform: 'rotate(90)'
+          }}
+          textProps={{
+            transform: 'rotate(270)'
+          }}
+        />
+        <div>some numbers</div>
       </div>
     );
   }
